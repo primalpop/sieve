0eedd0ed1952807ba25d260f7e44f84e711aa270
Primal Pappachan <primal.pappachan@gmail.com>
2018-04-19 19:03:51 -0700

    moving cost estimation to objectcondition

--- src/main/java/edu/uci/ics/tippers/model/policy/ObjectCondition.java
+++ src/main/java/edu/uci/ics/tippers/model/policy/ObjectCondition.java
@@ -1,6 +1,10 @@
 package edu.uci.ics.tippers.model.policy;
 
 import edu.uci.ics.tippers.common.AttributeType;
+import edu.uci.ics.tippers.common.PolicyConstants;
+import edu.uci.ics.tippers.common.PolicyEngineException;
+import edu.uci.ics.tippers.model.guard.Bucket;
+import edu.uci.ics.tippers.model.guard.Histogram;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,4 +35,54 @@ public class ObjectCondition extends BooleanCondition {
         booleanPredicates.add(new BooleanPredicate(o2, v2));
         this.booleanPredicates = booleanPredicates;
     }
+
+    public double computeL(){
+        List mBuckets = null;
+        double selectivity = 0.0001;
+        if(this.getAttribute().equalsIgnoreCase(PolicyConstants.TEMPERATURE_ATTR) ||
+                this.getAttribute().equalsIgnoreCase(PolicyConstants.ENERGY_ATTR)){
+            for (int i = 0; i < Histogram.getInstance().getBucketMap().get(this.getAttribute()).size(); i++) {
+                Bucket b = Histogram.getInstance().getBucketMap().get(this.getAttribute()).get(i);
+                if (Integer.parseInt(b.getValue()) >=
+                        Integer.parseInt(this.getBooleanPredicates().get(0).getValue())){
+                    selectivity += b.getFreq();
+                }
+            }
+        }
+        else if (this.getAttribute().equalsIgnoreCase(PolicyConstants.LOCATIONID_ATTR) ||
+                this.getAttribute().equalsIgnoreCase(PolicyConstants.ACTIVITY_ATTR)){
+            Bucket bucket = Histogram.getInstance().getBucketMap().get(this.getAttribute()).stream()
+                    .filter(b -> b.getValue().equalsIgnoreCase(this.getBooleanPredicates().get(0).getValue()))
+                    .findFirst()
+                    .orElse(null);
+            if (bucket != null) selectivity += bucket.getFreq();
+
+        }
+        else if (this.getAttribute().equalsIgnoreCase(PolicyConstants.USERID_ATTR)){
+            Bucket bucket = Histogram.getInstance().getBucketMap().get(this.getAttribute()).stream()
+                    .filter(b -> Integer.parseInt(b.getLower()) >=
+                            Integer.parseInt(this.getBooleanPredicates().get(0).getValue())
+                            && Integer.parseInt(b.getUpper()) <=
+                            Integer.parseInt(this.getBooleanPredicates().get(1).getValue()))
+                    .findFirst()
+                    .orElse(null);
+            if(bucket != null) selectivity += bucket.getFreq()/bucket.getNumberOfItems();
+
+        }
+        else if (this.getAttribute().equalsIgnoreCase(PolicyConstants.TIMESTAMP_ATTR)){
+            //TODO: Overestimates the selectivity as the partially contained buckets are completely counted
+            for (int i = 0; i < Histogram.getInstance().getBucketMap().get(this.getAttribute()).size(); i++) {
+                Bucket b = Histogram.getInstance().getBucketMap().get(this.getAttribute()).get(i);
+                if (timestampStrToCal(b.getLower()).compareTo(timestampStrToCal(this.getBooleanPredicates().get(1).getValue())) < 0
+                        && timestampStrToCal(b.getUpper()).compareTo(timestampStrToCal(this.getBooleanPredicates().get(0).getValue())) > 0){
+                    selectivity += b.getFreq();
+                }
+            }
+        }
+        else {
+            throw new PolicyEngineException("Unknown attribute");
+        }
+        return selectivity/100; //As the frequency is in percentage, to convert it to ratio
+    }
+
 }
