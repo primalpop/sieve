67226c84a341022fb544b67835a95c15de043b9b
Primal Pappachan <primal.pappachan@gmail.com>
2019-02-06 18:03:13 -0800

    factor search class

---
+++ src/main/java/edu/uci/ics/tippers/model/guard/FactorSearch.java
@@ -0,0 +1,17 @@
+package edu.uci.ics.tippers.model.guard;
+
+import edu.uci.ics.tippers.model.policy.BEExpression;
+
+import java.util.List;
+
+public class FactorSearch {
+
+    //Original expression
+    BEExpression expression;
+
+    List<BEExpression> open;
+
+    List<BEExpression> closed;
+
+
+}
