772fe5dd1ce4025694356e7526082de27053267f
Primal Pappachan <primal.pappachan@gmail.com>
2019-12-30 19:58:00 -0800

    inline vs udf experiment

--- src/main/java/edu/uci/ics/tippers/execution/ExperimentInline.java
+++ src/main/java/edu/uci/ics/tippers/execution/ExperimentInline.java
@@ -3,50 +3,46 @@ package edu.uci.ics.tippers.execution;
 import edu.uci.ics.tippers.common.PolicyConstants;
 import edu.uci.ics.tippers.db.MySQLQueryManager;
 import edu.uci.ics.tippers.db.MySQLResult;
-import edu.uci.ics.tippers.manager.PolicyPersistor;
-import edu.uci.ics.tippers.model.policy.BEPolicy;
 
-import java.util.List;
 
 public class ExperimentInline {
 
-    int querier;
 
     public ExperimentInline(){
-        this.querier = 177;
+
     }
 
     /**
      * Experiment to determine the policy evaluation cost
-     * TODO: Isolate read_cost and eval_cost, the current constant for policy_eval includes readt_cost as well
+     * TODO: Isolate read_cost and eval_cost, the current constant for policy_eval includes read_cost as well
      * @param args
      */
     public static void main(String[] args){
-        PolicyPersistor polper = new PolicyPersistor();
         MySQLQueryManager mySQLQueryManager = new MySQLQueryManager();
         String query_predicates = ") Select * from polEval";
-        String querier = "177";
-        String guard_id = "'d62bdc8c-a081-4fe4-a5ca-38208d88847e'";
-        String guard = "WITH polEval as (Select * from PRESENCE USE INDEX() WHERE (start_date<= \"2018-02-02\") " +
-                "AND (start_date>= \"2018-02-01\")";
+        String querier = "7073";
+        String guard_id = "'a21c9919-6e54-4b02-90d8-cc80720833a9'";
+        String guard = "WITH polEval as (Select * from PRESENCE WHERE (start_date<= \"2018-02-05\") " +
+                "AND (start_date>= \"2018-02-03\")";
         String udf_query = " hybcheck(" + querier + ", "
                  + guard_id +  " , user_id, location_id, start_date, " +
                         "start_time, user_profile, user_group ) = 1";
         MySQLResult udfTime = mySQLQueryManager.runTimedQueryExp(guard + PolicyConstants.CONJUNCTION
-                + udf_query + query_predicates, 1);
+                + udf_query + query_predicates, 3);
         System.out.println("UDF time " + udfTime.getTimeTaken().toMillis());
-        String add_policy_1 = "((user_profile= \"facultys\") " +
-                "AND (start_date<= \"2018-04-30\") AND (start_date>= \"2018-02-01\") " +
-                "AND (start_time>= \"00:01:00\") AND (start_time<= \"23:59:00\"))";
-        String add_policy_2 = "((user_profile= \"graduates\") " +
-                "AND (start_date<= \"2018-03-30\") AND (start_date>= \"2018-02-01\") " +
-                "AND (start_time>= \"10:12:00\") AND (start_time<= \"23:59:00\"))";
+        String add_policy_1 = "((user_profile= \"faculty\") " +
+                "AND (start_date<= \"2018-04-30\") AND (start_date>= \"2018-02-28\") " +
+                "AND (start_time>= \"00:10:00\") AND (start_time<= \"10:59:00\"))";
+        String add_policy_2 = "((user_profile= \"graduate\") " +
+                "AND (location_id = \"3145-clwa-5231\")" +
+                "AND (start_date<= \"2018-03-30\") AND (start_date>= \"2018-02-15\") " +
+                "AND (start_time>= \"15:12:00\") AND (start_time<= \"23:59:00\"))";
 
-        for (int i = 50; i < 200; i=i+50) {
+        for (int i = 8; i < 200; i=i+10) {
             String query = guard + " AND ((user_profile= \"graduate\") " +
-                    "AND (start_date<= \"2018-06-30\") AND (start_date>= \"2018-05-30\") " +
+                    "AND (start_date<= \"2018-05-01\") AND (start_date>= \"2018-02-28\") " +
                     "AND (start_time>= \"08:00:00\") AND (start_time<= \"17:00:00\")) " +
-                    "OR ((user_profile= \"undergrad\") AND (start_date<= \"2018-01-30\") " +
+                    "OR ((location_id = \"3146-clwa-6217\") AND (start_date<= \"2018-01-30\") " +
                     "AND (start_date>= \"2018-02-01\") AND (start_time>= \"12:00:00\") AND (start_time<= \"17:00:00\"))  ";
             for (int j = 0; j < i; j++)
                 if(j%2 == 0)
@@ -54,18 +50,8 @@ public class ExperimentInline {
                 else
                     query += PolicyConstants.DISJUNCTION + add_policy_2;
             query += query_predicates;
-            long total = 0;
-            boolean first = false;
-            for (int k = 0; k < 3; k++) {
-                MySQLResult execTime = mySQLQueryManager.runTimedQueryExp(query, 1);
-                if(!first) {
-                    first = true;
-                    continue;
-                }
-                total += execTime.getTimeTaken().toMillis();
-            }
-            System.out.println(" Num of " + i + " policies: " + total/(2) + " per policy: " +
-                    (total/2)/((i+2)*119451.0));
+            MySQLResult execTime = mySQLQueryManager.runTimedQueryExp(query, 3);
+            System.out.println(" Num of " + i + " policies: " + execTime.getTimeTaken().toMillis());
         }
     }
 }
