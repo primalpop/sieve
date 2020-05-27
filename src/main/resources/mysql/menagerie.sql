be113cc2d3cb7226f40bf1312254b507f945a8c4
Primal Pappachan <primal.pappachan@gmail.com>
2020-05-27 13:48:16 -0700

    cleaned up connection files

--- src/main/resources/mysql/menagerie.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS queries  (
-  id INT NOT NULL AUTO_INCREMENT,
-  template INT NOT NULL,
-  query_statement TEXT NOT NULL,
-  selectivity float(13,8) NOT NULL,
-  selectivity_type varchar(64) NOT NULL,
-  inserted_at timestamp NOT NULL,
-  PRIMARY KEY (ID)
- ) ;
