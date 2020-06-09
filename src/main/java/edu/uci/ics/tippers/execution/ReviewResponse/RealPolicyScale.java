7ec89e35a101be26dad6b8b8835aea92cac589f9
Primal Pappachan <primal.pappachan@gmail.com>
2020-06-08 22:33:44 -0700

    new cumulative policy result test

--- src/main/java/edu/uci/ics/tippers/execution/ReviewResponse/RealPolicyScale.java
+++ src/main/java/edu/uci/ics/tippers/execution/ReviewResponse/RealPolicyScale.java
@@ -3,6 +3,7 @@ package edu.uci.ics.tippers.execution.ReviewResponse;
 import edu.uci.ics.tippers.common.PolicyConstants;
 import edu.uci.ics.tippers.db.QueryManager;
 import edu.uci.ics.tippers.db.QueryResult;
+import edu.uci.ics.tippers.execution.ExpResult;
 import edu.uci.ics.tippers.fileop.Writer;
 import edu.uci.ics.tippers.manager.PolicyPersistor;
 import edu.uci.ics.tippers.model.guard.GuardExp;
@@ -28,84 +29,77 @@ import java.util.*;
 
 public class RealPolicyScale {
 
+    private static final int EXP_REPETITIONS = 5;
+    private static final int RANDOM_SAMPLING =  3;
 
-    private static final int EXP_REPETITIONS = 1;
+    private static final List<Integer> users = new ArrayList<>(Arrays.asList(22636, 22995, 32467, 15007, 18575, 23422, 26801, 11815, 18094, 2039));
+    private static final List<Integer> xpoints = new ArrayList<>(Arrays.asList(75, 100, 125, 150, 175, 200, 225, 250, 275, 300));
+
+    private static QueryManager queryManager;
+
+    private static long baseline(BEExpression beExpression) {
+        long baseline = 0;
+        if(PolicyConstants.DBMS_CHOICE.equalsIgnoreCase(PolicyConstants.MYSQL_DBMS)) {
+            String polIndexQuery = beExpression.createIndexQuery();
+            QueryResult indResult = queryManager.runTimedQueryExp(polIndexQuery, EXP_REPETITIONS);
+            System.out.println("Index rewrite Result count: " + indResult.getResultCount());
+            baseline += indResult.getTimeTaken().toMillis();
+        }
+        else {
+            String polEvalQuery = "With polEval as ( Select * from PRESENCE where " + beExpression.createQueryFromPolices() + "  )" ;
+            QueryResult tradResult = queryManager.runTimedQueryExp(polEvalQuery + "SELECT * from polEval ", EXP_REPETITIONS);
+            System.out.println("Vanilla rewrite Result count: " + tradResult.getResultCount());
+            baseline += tradResult.getTimeTaken().toMillis();
+        }
+        return baseline;
+    }
 
     public static void main(String[] args){
-
-        List<Integer> u1 = new ArrayList<>(Arrays.asList(10586, 32558, 21587, 13136, 13003, 34376, 10983));
-        List <Integer> u2 = new ArrayList<>(Arrays.asList(12096, 23957, 32804, 34755, 16371, 18112));
-        List<Integer> u3 = new ArrayList<>(Arrays.asList(20351, 23851, 36011, 11827, 14704, 22315));
-        List<Integer> u4 = new ArrayList<>(Arrays.asList(16436, 7683, 31432, 16880, 9001, 24492));
-        List<Integer> u5 = new ArrayList<>(Arrays.asList(10504, 26968, 22018, 18646, 18029, 29559));
-        List<Integer> u6 = new ArrayList<>(Arrays.asList(15135, 3832, 33791, 2469, 25582, 20568));
-        List<Integer> u7 = new ArrayList<>(Arrays.asList(25145, 32738, 19596, 5936, 6916, 16749));
-        List<Integer> u8 = new ArrayList<>(Arrays.asList(16915, 4817, 12054, 4355, 30276, 6815));
-        List<Integer> u9 = new ArrayList<>(Arrays.asList(22713, 14886, 26073, 13353, 16620, 14931));
-        List<Integer> u10 = new ArrayList<>(Arrays.asList(22636, 22995, 15007, 18575, 23422, 26801));
-
-        Map<Integer, List<Integer>> userMap = new TreeMap<>();
-        userMap.put(75, u1);
-        userMap.put(110, u2);
-        userMap.put(135, u3);
-        userMap.put(160, u4);
-        userMap.put(185, u5);
-        userMap.put(210, u6);
-        userMap.put(235, u7);
-        userMap.put(260, u8);
-        userMap.put(285, u9);
-        userMap.put(310, u10);
-
         PolicyConstants.initialize();
+        queryManager = new QueryManager();
         System.out.println("Running on " + PolicyConstants.DBMS_CHOICE + " at " + PolicyConstants.DBMS_LOCATION + " with "
                 +  PolicyConstants.TABLE_NAME + " and " + PolicyConstants.getNumberOfTuples() + " tuples");
-        QueryManager queryManager = new QueryManager();
+        Map<Integer, List<ExpResult>> resultMap = new TreeMap<>();
         String RESULTS_FILE = PolicyConstants.DBMS_CHOICE + "_results.csv";
-        PolicyPersistor polper = PolicyPersistor.getInstance();
-        String file_header = "Number Of Policies,Number of Guards,BaselineP,";
-        if(PolicyConstants.DBMS_CHOICE.equalsIgnoreCase(PolicyConstants.MYSQL_DBMS))
-            file_header +="BaselineIndex,";
-        file_header += "Sieve(with Union)\n";
+        String file_header = "Number Of Policies,Number of Guards,Baseline,Sieve\n";
         Writer writer = new Writer();
         writer.writeString(file_header, PolicyConstants.BE_POLICY_DIR, RESULTS_FILE);
-        for (int u:userMap.keySet()) {
-            long baselineP = 0, baselineI = 0, sieve = 0;
-            int numberOfGuards = 0;
-            List<Integer> segUsers = userMap.get(u);
-            StringBuilder rString = new StringBuilder();
-            for (int i = 0; i <  segUsers.size(); i++) {
-                String querier = String.valueOf(segUsers.get(i));
-                List<BEPolicy> bePolicies = polper.retrievePolicies(querier, PolicyConstants.USER_INDIVIDUAL, PolicyConstants.ACTION_ALLOW);
-                BEExpression beExpression = new BEExpression(bePolicies);
-                //Baseline Policies
-                String polEvalQuery = "With polEval as ( Select * from PRESENCE where " + beExpression.createQueryFromPolices() + "  )" ;
-                QueryResult tradResult = queryManager.runTimedQueryExp(polEvalQuery + "SELECT * from polEval ", EXP_REPETITIONS);
-                System.out.println("Vanilla rewrite Result count: " + tradResult.getResultCount());
-                baselineP += tradResult.getTimeTaken().toMillis();
-                //Baseline Index Policies only for MySQL
-                if(PolicyConstants.DBMS_CHOICE.equalsIgnoreCase(PolicyConstants.MYSQL_DBMS)) {
-                    String polIndexQuery = beExpression.createIndexQuery();
-                    QueryResult indResult = queryManager.runTimedQueryExp(polIndexQuery, EXP_REPETITIONS);
-                    System.out.println("Index rewrite Result count: " + indResult.getResultCount());
-                    baselineI += indResult.getTimeTaken().toMillis();
+
+        PolicyPersistor polper = PolicyPersistor.getInstance();
+        for (Integer user : users) {
+            String querier = String.valueOf(user);
+            List<BEPolicy> bePolicies = polper.retrievePolicies(querier, PolicyConstants.USER_INDIVIDUAL, PolicyConstants.ACTION_ALLOW);
+            for (int k = 0; k < RANDOM_SAMPLING; k++) {
+                Collections.shuffle(bePolicies);
+                for (Integer xpoint : xpoints) {
+                    List<BEPolicy> subPolicies = bePolicies.subList(0, xpoint);
+                    ExpResult expResult = new ExpResult();
+                    expResult.setNumberOfPolicies(subPolicies.size());
+                    BEExpression beExpression = new BEExpression(subPolicies);
+                    expResult.setBaselineR(baseline(beExpression));
+                    //Guard Generation
+                    SelectGuard gh = new SelectGuard(beExpression, true);
+                    System.out.println("Number of policies: " + beExpression.getPolicies().size() + " Number of Guards: " + gh.numberOfGuards());
+                    expResult.setNumberOfGuards(gh.numberOfGuards());
+                    GuardExp guardExp = gh.create();
+                    String guard_query_union = guardExp.queryRewrite(true, true);
+                    QueryResult execResultUnion = queryManager.runTimedQueryExp(guard_query_union, EXP_REPETITIONS);
+                    expResult.setSieveR(execResultUnion.getTimeTaken().toMillis());
+                    if (!resultMap.containsKey(xpoint)) {
+                        List<ExpResult> expResults = new ArrayList<>();
+                        expResults.add(expResult);
+                        resultMap.put(xpoint, expResults);
+                    } else resultMap.get(xpoint).add(expResult);
                 }
-                //Guard Generation
-                SelectGuard gh = new SelectGuard(beExpression, true);
-                System.out.println("Number of policies: " + beExpression.getPolicies().size() + " Number of Guards: " + gh.numberOfGuards());
-                numberOfGuards += gh.numberOfGuards();
-                GuardExp guardExp = gh.create();
-                String guard_query_union = guardExp.queryRewrite(true, true);
-                QueryResult execResultUnion = queryManager.runTimedQueryExp(guard_query_union, EXP_REPETITIONS);
-                sieve += execResultUnion.getTimeTaken().toMillis();
-                System.out.println("Guard Query execution (with Union): "  + " Time: " + execResultUnion.getTimeTaken().toMillis());
-                System.out.println("Guard Query result count (with Union): "  + " Time: " + execResultUnion.getResultCount());
             }
-            rString.append(u).append(",");
-            rString.append(numberOfGuards/segUsers.size()).append(",");
-            rString.append(baselineP/segUsers.size()).append(",");
-            if(PolicyConstants.DBMS_CHOICE.equalsIgnoreCase(PolicyConstants.MYSQL_DBMS))
-                rString.append(baselineI/segUsers.size()).append(",");
-            rString.append(sieve/segUsers.size()).append("\n");
+        }
+        for (int x: resultMap.keySet()) {
+            StringBuilder rString = new StringBuilder();
+            OptionalDouble baselineR = resultMap.get(x).stream().mapToLong(ExpResult::getBaselineR).average();
+            OptionalDouble sieveR = resultMap.get(x).stream().mapToLong(ExpResult::getSieveR).average();
+            OptionalDouble numOfGuards = resultMap.get(x).stream().mapToInt(ExpResult::getNumberOfGuards).average();
+            rString.append(x).append(",").append(String.format("%.2f", baselineR.getAsDouble())).append(",").append(String.format("%.2f", sieveR.getAsDouble())).append(",")
+                    .append(Math.ceil(numOfGuards.getAsDouble())).append("\n");
             writer.writeString(rString.toString(), PolicyConstants.BE_POLICY_DIR, RESULTS_FILE);
         }
     }
